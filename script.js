75faf332b7487df30e28f072c669b5b08bda4311
Lucas Correia <lucas_p_correia_cadmus@whirlpool.com>
2025-03-03 15:14:09 -0300

    feat: player moviment

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const gravity = 0.5;
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+};
 
 class Player {
   constructor() {
@@ -27,6 +35,7 @@ class Player {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
@@ -45,6 +54,58 @@ function animate() {
   c.clearRect(0, 0, canvas.width, canvas.height);
 
   player.update();
+
+  if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
 }
 
 animate();
+
+window.addEventListener("keydown", ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      keys.left.pressed = true;
+      break;
+
+    case 83:
+      break;
+
+    case 68:
+      keys.right.pressed = true;
+      break;
+
+    case 87:
+      player.velocity.y -= 20;
+      break;
+
+    default:
+      break;
+  }
+});
+
+window.addEventListener("keyup", ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      keys.left.pressed = false;
+      break;
+
+    case 83:
+      break;
+
+    case 68:
+      keys.right.pressed = false;
+      break;
+
+    case 87:
+      player.velocity.y -= 20;
+      break;
+
+    default:
+      break;
+  }
+});
