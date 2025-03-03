26e6aab00d10d0b9afd64ab173cbe5d94186f7a9
Lucas Correia <lucas_p_correia_cadmus@whirlpool.com>
2025-03-03 15:35:33 -0300

    feat: scroll the background

--- script.js
+++ script.js
@@ -47,10 +47,10 @@ class Player {
 }
 
 class Platform {
-  constructor() {
+  constructor({ x, y }) {
     this.position = {
-      x: 200,
-      y: 300,
+      x,
+      y,
     };
     this.width = 200;
     this.height = 20;
@@ -63,7 +63,10 @@ class Platform {
 }
 
 const player = new Player();
-const platform = new Platform();
+const platforms = [
+  new Platform({ x: 200, y: 100 }),
+  new Platform({ x: 500, y: 300 }),
+];
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -71,25 +74,37 @@ function animate() {
   c.clearRect(0, 0, canvas.width, canvas.height);
 
   player.update();
-  platform.draw();
+  platforms.forEach((platform) => {
+    platform.draw();
+  });
 
-  if (keys.right.pressed) {
+  if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = 5;
-  } else if (keys.left.pressed) {
+  } else if (keys.left.pressed && player.position.x > 100) {
     player.velocity.x = -5;
   } else {
     player.velocity.x = 0;
+
+    platforms.forEach((platform) => {
+      if (keys.right.pressed) {
+        platform.position.x -= 5;
+      } else if (keys.left.pressed) {
+        platform.position.x += 5;
+      }
+    });
   }
 
-  if (
-    player.position.y + player.height <= platform.position.y &&
-    player.position.y + player.height + player.velocity.y >=
-      platform.position.y &&
-    player.position.x + player.width >= platform.position.x &&
-    player.position.x <= platform.position.x + platform.width
-  ) {
-    player.velocity.y = 0;
-  }
+  platforms.forEach((platform) => {
+    if (
+      player.position.y + player.height <= platform.position.y &&
+      player.position.y + player.height + player.velocity.y >=
+        platform.position.y &&
+      player.position.x + player.width >= platform.position.x &&
+      player.position.x <= platform.position.x + platform.width
+    ) {
+      player.velocity.y = 0;
+    }
+  });
 }
 
 animate();
