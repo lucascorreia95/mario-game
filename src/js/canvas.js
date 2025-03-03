55caa32eb954c48490586dbda428f4e5cb4c24e6
Lucas Correia <lucas_p_correia_cadmus@whirlpool.com>
2025-03-03 16:14:45 -0300

    feat: image platforms

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,8 +1,10 @@
+import platform from "../img/platform.png";
+
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 1024;
+canvas.height = 576;
 
 const gravity = 0.5;
 const keys = {
@@ -48,36 +50,41 @@ class Player {
 }
 
 class Platform {
-  constructor({ x, y }) {
+  constructor({ x, y, image }) {
     this.position = {
       x,
       y,
     };
-    this.width = 200;
-    this.height = 20;
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
   }
 
   draw() {
-    c.fillStyle = "blue";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    c.drawImage(this.image, this.position.x, this.position.y);
   }
 }
 
+const image = new Image();
+image.src = platform;
+
 const player = new Player();
 const platforms = [
-  new Platform({ x: 200, y: 100 }),
-  new Platform({ x: 500, y: 300 }),
+  new Platform({ x: -1, y: 470, image }),
+  new Platform({ x: image.width - 3, y: 470, image }),
 ];
 
 function animate() {
   window.requestAnimationFrame(animate);
 
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
-  player.update();
   platforms.forEach((platform) => {
     platform.draw();
   });
+
+  player.update();
 
   if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = 5;
